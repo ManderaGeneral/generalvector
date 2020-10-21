9750f5dadadbcc8a2a29ca3bfbbc9f6d9852f190
Mandera <krizoorn@hotmail.com>
2020-10-21 17:42:45 +0200

    Requests

--- randomtesting.py
+++ randomtesting.py
@@ -9,10 +9,15 @@ from generallibrary import getLocalFeaturesAsMD
 # print(getLocalFeaturesAsMD(locals(), "generalvector"))
 
 
-d = dict(Vec2.__dict__)
-d.update(dict(Vec.__dict__))
-d.update(dict(GeneralVector.__dict__))
+# d = dict(Vec2.__dict__)
+# d.update(dict(Vec.__dict__))
+# d.update(dict(GeneralVector.__dict__))
+#
+# print(getLocalFeaturesAsMD(d, "generalvector"))
 
 
-print(getLocalFeaturesAsMD(d, "generalvector"))
+import requests
+import re
+
+print(" ".join(re.findall("/project/(.*)/", requests.get("https://pypi.org/user/Mandera/").text)))
 
