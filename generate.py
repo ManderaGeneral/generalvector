49ec18381f68c841c61fa4ef3befbc2d3a5685ab
runner <runner@fv-az193-207>
2022-09-29 13:19:42 +0000

    [CI AUTO] Sync triggered by generalpackager

--- generate.py
+++ generate.py
@@ -1,7 +1,8 @@
 
 from generalpackager import Packager
 
-Packager("generalvector").generate_localfiles(print_out=True)
+if __name__ == "__main__":
+    Packager("generalvector").generate_localfiles(print_out=True)
 
 
 
